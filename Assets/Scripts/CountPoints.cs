0913f7525634f9ab9d9f2e176a73842b57d2a340
Jucielly silva <juciellysouzasilva@gmail.com>
2023-05-14 13:54:47 -0300

    adicionei hud peças e tempo

--- Assets/Scripts/CountPoints.cs
+++ Assets/Scripts/CountPoints.cs
@@ -12,6 +12,6 @@ public class CountPoints : MonoBehaviour
 
     private void Update()
     {
-        textPoints.text = points.ToString();
+        textPoints.text = points.ToString() + "/16";
     }
 }
